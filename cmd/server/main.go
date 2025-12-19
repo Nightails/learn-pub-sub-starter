81d8a6e2f789576fc575452459b68d07a0332129
a_nightails <a.nightails@gmail.com>
2025-12-18 20:26:22 -0600

    Add RabbitMQ support and update configurations
    - Integrate RabbitMQ client library for messaging.
    - Update `.gitignore` with additional exclusions.
    - Add `Dockerfile` for RabbitMQ setup.
    - Include RabbitMQ dependencies in `go.mod` and `go.sum`.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	url := "amqp://guest:guest@localhost:5672/"
+	conn, _ := amqp.Dial(url)
+	defer conn.Close()
+	fmt.Println("Successfully connected to the server")
+
+	ch, _ := conn.Channel()
+	defer ch.Close()
+
+	// wait for ctrl+c
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+	fmt.Println("Shutting down and closing connection...")
 }
